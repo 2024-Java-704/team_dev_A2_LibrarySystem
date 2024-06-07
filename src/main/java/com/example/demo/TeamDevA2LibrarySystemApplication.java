514e57d39ae249fb920f0a8b93176c1d7e992f90
kobayashi-taiyo-6493 <420-student059@la-study.com>
2024-06-07 09:15:38 +0900

    first commit

---
+++ src/main/java/com/example/demo/TeamDevA2LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDevA2LibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDevA2LibrarySystemApplication.class, args);
+	}
+
+}
