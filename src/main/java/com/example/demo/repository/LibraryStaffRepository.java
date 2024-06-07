a02f882f5c3e3b675a62f74a5c736b3046eff8f4
yamazaki-yu-6383 <420-student043@la-study.com>
2024-06-07 17:27:56 +0900

    職員用ログイン画面の作成（ログイン後の画面との接続はまだ）

--- src/main/java/com/example/demo/repository/LibraryStaffRepository.java
+++ src/main/java/com/example/demo/repository/LibraryStaffRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.LibraryStaff;
 
 public interface LibraryStaffRepository extends JpaRepository<LibraryStaff, Integer> {
+
+	List<LibraryStaff> findByEmailAndPassword(String email, String password);
 
 }
