075a32c31062084947042bc6b568af3e3f5eeb0c
kobayashi-taiyo-6493 <420-student059@la-study.com>
2024-06-07 13:01:18 +0900

    first commit

--- src/main/java/com/example/demo/entity/Reservation.java
+++ src/main/java/com/example/demo/entity/Reservation.java
@@ -1,15 +1,14 @@
 package com.example.demo.entity;
 
 
-import java.awt.print.Book;
-import java.time.LocalDate;
 
-import org.apache.tomcat.jni.Library;
+import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
