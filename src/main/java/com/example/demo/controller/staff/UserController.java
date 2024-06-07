c83c53ea02f65db971ddc26b276a6ff04e2196d0
oizumi-yusei-6490 <420-student002@la-study.com>
2024-06-07 17:35:42 +0900

    komittoto

--- src/main/java/com/example/demo/controller/staff/UserController.java
+++ src/main/java/com/example/demo/controller/staff/UserController.java
@@ -1,14 +1,15 @@
 package com.example.demo.controller.staff;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 	
 	@GetMapping("/staff/userMg")
-	public String index(
+	public String index(Model model
 			) {
-		return "userManagement";
+		return "/staff/userManagement";
 	}
 }
