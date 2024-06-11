8d5b728af1c0378a34745546688a1f7d7323d65f
kaneko-takumi-0130 <420-student046@la-study.com>
2024-06-11 11:11:03 +0900

    注文の追加

--- src/main/java/com/example/demo/entity/Reservation.java
+++ src/main/java/com/example/demo/entity/Reservation.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
 
-
-
 import java.time.LocalDate;
 
 import jakarta.persistence.Column;
@@ -20,36 +18,50 @@ public class Reservation {
 	@Id //主キー
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //自動採番
 	private Integer id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "book_id")
 	private Book book;
-	
+
 	@Column(name = "reservation_date")
 	private LocalDate reservationDate;
-	
+
 	@Column(name = "scheduled_date")
 	private LocalDate scheduledDate;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "library_id")
 	private Library library;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "status_id")
 	private Status status;
-	
-	public Reservation(){	
+
+	public Reservation() {
 	}
-	public Reservation(User user, Book book){
+
+	public Reservation(User user, Book book) {
 		this.user = user;
 		this.book = book;
 	}
-	public Reservation(Integer id, User user, Book book, LocalDate reservationDate, LocalDate scheduledDate, Library library, Status status){
+
+	public Reservation(User user, Book book, LocalDate reservationDate, LocalDate scheduledDate,
+			Library library, Status status) {
+
+		this.user = user;
+		this.book = book;
+		this.reservationDate = reservationDate;
+		this.scheduledDate = scheduledDate;
+		this.library = library;
+		this.status = status;
+	}
+
+	public Reservation(Integer id, User user, Book book, LocalDate reservationDate, LocalDate scheduledDate,
+			Library library, Status status) {
 		this.id = id;
 		this.user = user;
 		this.book = book;
@@ -58,47 +70,61 @@ public class Reservation {
 		this.library = library;
 		this.status = status;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public Book getBook() {
 		return book;
 	}
+
 	public void setBook(Book book) {
 		this.book = book;
 	}
+
 	public LocalDate getReservationDate() {
 		return reservationDate;
 	}
+
 	public void setReservationDate(LocalDate reservationDate) {
 		this.reservationDate = reservationDate;
 	}
+
 	public LocalDate getScheduledDate() {
 		return scheduledDate;
 	}
+
 	public void setScheduledDate(LocalDate scheduledDate) {
 		this.scheduledDate = scheduledDate;
 	}
+
 	public Library getLibrary() {
 		return library;
 	}
+
 	public void setLibrary(Library library) {
 		this.library = library;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
-	
+
 }
