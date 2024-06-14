9116fa5bf15e02439c2864a25c70f943927a3138
kaneko-takumi-0130 <420-student046@la-study.com>
2024-06-14 10:56:24 +0900

    検索

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -5,15 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.Book;
 import com.example.demo.entity.Category;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
-	//タイトルあいまい検索
+	// タイトルあいまい検索
 	List<Book> findByTitleContaining(String title);
 
 	List<Book> findByCategory(Category category);
@@ -36,16 +34,15 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 
 	List<Book> findByIdAndCategoryId(Integer bookId, Integer categoryId);
 
-	@Query("SELECT b FROM Book b WHERE " +
-			"(:title IS NULL OR b.title LIKE %:title%) AND " +
-			"(:categoryId IS NULL OR b.category.id = :categoryId) AND " +
-			"(:author IS NULL OR b.author LIKE %:author%) AND " +
-			"(:publisher IS NULL OR b.publisher LIKE %:publisher%) AND " +
-			"(:pubYear IS NULL OR b.pubYear = :pubYear)")
-	List<Book> searchBooks(@Param("title") String title,
-			@Param("categoryId") Integer categoryId,
-			@Param("author") String author,
-			@Param("publisher") String publisher,
-			@Param("pubYear") LocalDate pubYear);
+	// ここから追加分
+	List<Book> findByTitleNotContaining(String title);
+
+	List<Book> findByAuthorNotContaining(String author);
+
+	List<Book> findByPublisherNotContaining(String publisher);
+
+	List<Book> findByCategoryNotIn(List<Category> categories); // 修正点
+
+	List<Book> findByPubYearNotIn(List<LocalDate> pubYears);
 
 }
